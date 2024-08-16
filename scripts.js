c88a22c054d92d642d6c98db65841781d0a15037
claudiahW <murungiclaud@gmail.com>
2024-08-16 21:38:00 +0300

    write a function to change carousel images on hero upon click of arrows

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const imgs = document.querySelectorAll(".header-slider ul img");
 const prev_btn = document.querySelectorAll(".control_prev");
 const next_btn = document.querySelectorAll(".control_next");
 
-let n = 1;
+let n = 0;
 
 function changeSlide() {
   for (let i = 0; i < imgs.length; i++) {
@@ -11,3 +11,11 @@ function changeSlide() {
   imgs[n].style.display = "block";
 }
 changeSlide();
+
+prev_btn.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+});
