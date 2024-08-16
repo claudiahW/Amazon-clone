a731b5e68bec6e9280fbd7fcf44afda8a59620ae
claudiahW <murungiclaud@gmail.com>
2024-08-17 00:14:38 +0300

    style products section to scroll easily

--- scripts.js
+++ scripts.js
@@ -29,3 +29,14 @@ next_btn.addEventListener("click", (e) => {
   }
   changeSlide();
 });
+
+//sroll functionality
+
+const scrollContainer = document.querySelectorAll(".products");
+
+for (const item of scrollContainer) {
+  item.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    item.scrollLeft += evt.deltaY;
+  });
+}
